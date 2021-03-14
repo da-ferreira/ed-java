0772ef7ce092ddfbdba493c6ef82357a256b24c5
David Ferreira de Almeida <72570193+da-ferreira@users.noreply.github.com>
2021-03-14 12:04:40 -0300

    Add files via upload

--- Atividades Continuas/AC_02/src/tests/InverteArranjoTest.java
+++ Atividades Continuas/AC_02/src/tests/InverteArranjoTest.java
@@ -2,13 +2,34 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import exercicio_04.InverteArranjo;
 
 class InverteArranjoTest {
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testaInverterArranjo() {
+		int array[] = {1, 2, 3, 4, 5};
+		
+		assertEquals("[1, 2, 3, 4, 5]", Arrays.toString(array), "Deve imprimir [1, 2, 3, 4, 5]");
+		InverteArranjo.inverter(array);
+		assertEquals("[5, 4, 3, 2, 1]", Arrays.toString(array), "Deve imprimir [5, 4, 3, 2, 1]");
+		assertEquals(5, array[0], "Deve imprimir 5");
+		
+		int array1[] = {15, 14, 13, 12, 11};
+		
+		assertEquals("[15, 14, 13, 12, 11]", Arrays.toString(array1), "Deve imprimir [15, 14, 13, 12, 11]");
+		InverteArranjo.inverter(array1);
+		assertEquals("[11, 12, 13, 14, 15]", Arrays.toString(array1), "Deve imprimir [11, 12, 13, 14, 15]");
+		assertEquals(11, array1[0], "Deve imprimir 11");
+		
+		int array2[] = {2, 4, 6};
+		
+		assertEquals("[2, 4, 6]", Arrays.toString(array2), "Deve imprimir [2, 4, 6]");
+		InverteArranjo.inverter(array2);
+		assertEquals("[6, 4, 2]", Arrays.toString(array2), "Deve imprimir [6, 4, 2]");
+		assertEquals(6, array2[0], "Deve imprimir 6");
 	}
 
 }
