9991c023b4d0c0bb5bf14bc640e1ea73309fc501
David Ferreira de Almeida <72570193+da-ferreira@users.noreply.github.com>
2021-03-17 15:08:48 -0300

    Add files via upload

--- 08-ListaDeNodos/src/lista_de_nodos/EmptyListException.java
+++ 08-ListaDeNodos/src/lista_de_nodos/EmptyListException.java
@@ -1,7 +1,7 @@
 
 package lista_de_nodos;
 
-//Excecao lançada se a lista estiver e for feita alguma operação (ex: pegar o primeiro elemento da lista).
+//Excecao lançada se a lista estiver vazia e for feita alguma operação (ex: pegar o primeiro elemento da lista).
 
 @SuppressWarnings("serial")
 public class EmptyListException extends RuntimeException {
