47908e880e82056bd763623884e2696f33f4f952
David Ferreira de Almeida <72570193+da-ferreira@users.noreply.github.com>
2021-03-26 15:17:55 -0300

    Add files via upload

--- 10-ArvoreBinaria/src/arvore_binaria/LinkedBinaryTree.java
+++ 10-ArvoreBinaria/src/arvore_binaria/LinkedBinaryTree.java
@@ -6,7 +6,16 @@ import java.util.Iterator;
 import exceptions.BoundaryViolationException;
 import exceptions.EmptyTreeException;
 import exceptions.InvalidPositionException;
+import exceptions.NonEmptyTreeException;
 import lista_de_nodos.NodePositionList;
+
+/*
+ * public Iterable<BTNode<Type>> positions()
+ *
+ * public Iterator<Type> iterator()
+ * 
+ * public Type remove(BTNode<Type> node) throws InvalidPositionException
+ */
 
 /**
  * Classe que implementa um árvore binária, onde cada nó da árvore pode ter até 2 filhos.
@@ -120,6 +129,90 @@ public class LinkedBinaryTree<Type> implements BinaryTree<Type> {
 	
 	/* MÉTODOS ADICIONAIS */
 	
+	/**
+	 * @param node: Nó que se deseja saber seu irmão (esquerdo ou direito).
+	 * @return Retorna um irmão de um dado nó.
+	 * @throws InvalidPositionException: Caso a posição (node) seja inválida.
+	 * @throws BoundaryViolationException: Caso o nó não tenha irmão.
+	 */
+	public BTNode<Type> sibling(BTNode<Type> node) throws InvalidPositionException, BoundaryViolationException {
+		BTNode<Type> point = checkPosition(node);
+		BTNode<Type> parentPoint = point.getParent();
+		
+		if (parentPoint != null) {
+			BTNode<Type> sibling;
+			
+			if (point == parentPoint.getLeft()) {
+				sibling = parentPoint.getRight();
+			}
+			else {
+				sibling = parentPoint.getLeft();
+			}
+			
+			if (sibling != null)
+				return sibling;
+		}
+		
+		throw new BoundaryViolationException("The node has no sibling");
+	}
+	
+	/**
+	 * Adiciona uma raíz em uma árvore binária vazia.
+	 * @param element: Raíz
+	 * @return O nó que contem a raíz.
+	 * @throws NonEmptyTreeException: Caso a árvore não esteja vazia.
+	 */
+	public BTNode<Type> addRoot(Type element) throws NonEmptyTreeException {
+		if (this.size != 0)
+			throw new NonEmptyTreeException("The tree is not empty!");
+		
+		root = new BTNode<Type>(element, null, null, null);
+		size = 1;
+		return root;
+	}
+	
+	/**
+	 * Insere um elemento na esquerda de uma dado nó.
+	 * @param node: O nó que se deseja inserir o elemento a esquerda.
+	 * @param element: O elemento que se deseja inserir.
+	 * @return O nó do elemento inserido.
+	 * @throws InvalidPositionException: Caso o nó passado já tenha um filho a esquerda.
+	 */
+	public BTNode<Type> insertLeft(BTNode<Type> node, Type element) throws InvalidPositionException {
+		BTNode<Type> point = checkPosition(node);
+		
+		if (point.getLeft() != null)
+			throw new InvalidPositionException("The node already has a child on the left");
+		
+		point.setLeft(new BTNode<Type>(element, null, null, point));
+		size++;
+		return point.getLeft();
+	}
+	
+	/**
+	 * Insere um elemento na direita de uma dado nó.
+	 * @param node: O nó que se deseja inserir o elemento a direita.
+	 * @param element: O elemento que se deseja inserir.
+	 * @return O nó do elemento inserido.
+	 * @throws InvalidPositionException: Caso o nó passado já tenha um filho a direita.
+	 */
+	public BTNode<Type> insertRight(BTNode<Type> node, Type element) throws InvalidPositionException {
+		BTNode<Type> point = checkPosition(node);
+		
+		if (point.getRight() != null)
+			throw new InvalidPositionException("The node already has a child on the right");
+		
+		point.setRight(new BTNode<Type>(element, null, null, point));
+		size++;
+		return point.getRight();
+	}
+	
+	public Type remove(BTNode<Type> node) throws InvalidPositionException {
+		return null;
+	}
+	
+	
+
 	/**
 	 * Verifica se a posição de um dado BTNode é válida
 	 * @param node: O nó a ser verificado
